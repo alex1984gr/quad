b4d8132229489425282e2e0d7c91d30f8812a0e4
lpapanthy <deezolut@gmail.com>
2025-09-07 16:24:05 +0300

    Complete quadC

---
+++ quadC.go
@@ -0,0 +1,32 @@
+package piscine
+
+import "fmt"
+
+func QuadC(x,y int) {
+
+	if x <= 0 || y <=0 {
+		return
+	}
+
+	for row := 1; row <= y; row++ {
+		for col := 1; col <= x; col++ {
+
+			
+			if row == 1 && (col == 1 || col == x) {
+				fmt.Print("A")
+			} else if row == y && (col == 1 || col == x) {
+				fmt.Print("C")
+			} else if (row > 1 && row < y) && (col == 1 || col == x) {
+				fmt.Print("B")
+			} else if (row == 1 || row == y) && (col > 1 && col < x) {
+				fmt.Print("B")
+			} else if (row > 1 && row < y) && (col > 1 && col < x) {
+				fmt.Print(" ")
+			} 
+					
+		}
+
+		fmt.Print("\n")
+	}
+	
+}
